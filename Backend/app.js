471907ea8cb825c34bed663884da0ee72f23597a
Jacques Garcia <jack.garcia@outlook.fr>
2020-07-17 23:15:03 +0200

    ajout de divers plugin de protection

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express'); // Déclaration framework express
-const bodyParser = require('body-parser'); // Déclaration Body-Parser pour récupérer des objets exploitables
+const bodyParser = require('body-parser');// Déclaration Body-Parser pour récupérer des objets exploitables
+
+require('dotenv').config();
+const helmet = require('helmet');
 
 const userRoutes = require('./routes/user'); // Déclaration du dossier des routes utilisateur
 
 const app = express();
 
-// DECLARATION DE MA BASE DE DONNEES
 
 // DECLARATION DE MES CORS, HEADERS ET METHODS
 app.use((req, res, next) =>{
@@ -16,6 +18,7 @@ app.use((req, res, next) =>{
 });
 
 app.use(bodyParser.json());
+app.use(helmet());
 
 app.use('/api/auth', userRoutes);
 
