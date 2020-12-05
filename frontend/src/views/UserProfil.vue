306743498794b0aab1ec86730ac9e1c2d7f818a5
Jacques Garcia <jack.garcia@outlook.fr>
2020-12-05 07:41:58 +0100

    ajout de la page du profil utilisateur

---
+++ frontend/src/views/UserProfil.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="container">
+        <onlyBan />
+        <profilUser />
+    </div>
+</template>
+<script>
+import OnlyBan from "../components/OnlyBan"
+import ProfilUser from "../components/ProfilUser"
+
+export default {
+    name: "UserProfil",
+    components: {
+        OnlyBan,
+        ProfilUser
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
